5763da48e46a844f2876bd084e9970933ffe0d96
betosedenho <betosedenho@hotmail.com>
2022-01-18 18:19:21 -0300

    Relacionamento entre Cidades e Estados

--- src/main/java/com/roberto/petshop/utils/PopulaDados.java
+++ src/main/java/com/roberto/petshop/utils/PopulaDados.java
@@ -8,12 +8,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.roberto.petshop.domain.Categoria;
+import com.roberto.petshop.domain.Cidade;
 import com.roberto.petshop.domain.Especie;
+import com.roberto.petshop.domain.Estado;
 import com.roberto.petshop.domain.Pet;
 import com.roberto.petshop.domain.Produto;
 import com.roberto.petshop.domain.Raca;
 import com.roberto.petshop.repositories.CategoriaRepository;
+import com.roberto.petshop.repositories.CidadeRepository;
 import com.roberto.petshop.repositories.EspecieRepository;
+import com.roberto.petshop.repositories.EstadoRepository;
 import com.roberto.petshop.repositories.PetRepository;
 import com.roberto.petshop.repositories.ProdutoRepository;
 import com.roberto.petshop.repositories.RacaRepository;
@@ -35,6 +39,12 @@ public class PopulaDados {
 	
 	@Autowired
 	PetRepository petRepository;
+	
+	@Autowired
+	EstadoRepository estadoRepository;
+	
+	@Autowired
+	CidadeRepository cidadeRepository;
 	
 	@PostConstruct
 	public void cadastrar() {
@@ -74,6 +84,19 @@ public class PopulaDados {
 		especieRepository.saveAll(Arrays.asList(esp1, esp2));
 		racaRepository.saveAll(Arrays.asList(rac1, rac2, rac3));
 		petRepository.saveAll(Arrays.asList(pet1, pet2, pet3));
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Belo Horizonte", est1); 
+		Cidade c2 = new Cidade(null, "Capelinha", est1); 
+		Cidade c3 = new Cidade(null, "São Paulo", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1, c2));
+		est2.getCidades().addAll(Arrays.asList(c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 	}
 
 }
