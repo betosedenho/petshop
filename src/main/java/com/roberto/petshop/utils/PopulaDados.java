85e2f747a370716f5d7020471adfcf73e0d989f2
betosedenho <betosedenho@hotmail.com>
2022-01-18 17:16:00 -0300

    Criar classes Pet, Especie, Raca e seus respectivos Repositories

--- src/main/java/com/roberto/petshop/utils/PopulaDados.java
+++ src/main/java/com/roberto/petshop/utils/PopulaDados.java
@@ -8,9 +8,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.roberto.petshop.domain.Categoria;
+import com.roberto.petshop.domain.Especie;
+import com.roberto.petshop.domain.Pet;
 import com.roberto.petshop.domain.Produto;
+import com.roberto.petshop.domain.Raca;
 import com.roberto.petshop.repositories.CategoriaRepository;
+import com.roberto.petshop.repositories.EspecieRepository;
+import com.roberto.petshop.repositories.PetRepository;
 import com.roberto.petshop.repositories.ProdutoRepository;
+import com.roberto.petshop.repositories.RacaRepository;
 
 @Component
 public class PopulaDados {
@@ -20,6 +26,15 @@ public class PopulaDados {
 	
 	@Autowired
 	ProdutoRepository produtoRepository;
+	
+	@Autowired
+	EspecieRepository especieRepository;
+	
+	@Autowired
+	RacaRepository racaRepository;
+	
+	@Autowired
+	PetRepository petRepository;
 	
 	@PostConstruct
 	public void cadastrar() {
@@ -44,6 +59,21 @@ public class PopulaDados {
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3, p4));
+		
+		Especie esp1 = new Especie( null, "Cachorro");
+		Especie esp2 = new Especie( null, "Gato");
+		
+		Raca rac1 = new Raca(null, "Shitzu");
+		Raca rac2 = new Raca(null, "Akita");
+		Raca rac3 = new Raca(null, "Persa");
+		
+		Pet pet1 = new Pet(null, "John", 6, esp1, rac1);
+		Pet pet2 = new Pet(null, "Hana", 5, esp1, rac2);
+		Pet pet3 = new Pet(null, "Mewth", 8, esp2, rac3);
+		
+		especieRepository.saveAll(Arrays.asList(esp1, esp2));
+		racaRepository.saveAll(Arrays.asList(rac1, rac2, rac3));
+		petRepository.saveAll(Arrays.asList(pet1, pet2, pet3));
 	}
 
 }
